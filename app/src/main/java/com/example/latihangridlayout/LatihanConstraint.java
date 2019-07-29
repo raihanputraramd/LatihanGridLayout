12b2941eb24b7227a0bae6ce7981362e96e4ed04
rpr2911 <Raihan291101>
2019-07-29 11:28:12 +0700

    Initial commit

---
+++ app/src/main/java/com/example/latihangridlayout/LatihanConstraint.java
@@ -0,0 +1,16 @@
+package com.example.latihangridlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import static com.example.latihangridlayout.R.layout.activity_latihan_constraint;
+
+public class LatihanConstraint extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_latihan_constraint);
+    }
+}
